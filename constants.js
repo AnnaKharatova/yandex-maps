fabb35be6f7e0529addb4ed7fc797804a41a6340
AnnaKharatova <black-isida@yandex.ru>
2024-06-12 01:42:34 +0300

    изменен внешний вид фильтрации по партнерам

---
+++ constants.js
@@ -0,0 +1,127 @@
+export const storeList = [
+    {
+        "id": 1,
+        "parts_available": [
+            {
+                "name": "YAMZ"
+            },
+            {
+                "name": "KAMAZ_R6"
+            },
+            {
+                "name": "Cummins_6.7"
+            }
+        ],
+        "name": "РТИ",
+        "image": null,
+        "address": "Чебоксары, улица Цивильская, дом 13",
+        "latitude": "56.124145",
+        "longitude": "47.271371",
+        "phone": "4545454",
+        "website": "",
+        "tags": []
+    },
+    {
+        "id": 2,
+        "parts_available": [
+            {
+                "name": "Cummins_6.7"
+            }
+        ],
+        "name": "КАМРТИ",
+        "image": null,
+        "address": "Транспортная ул., 1, Балаково, Саратовская обл., 413859",
+        "latitude": "52.000700",
+        "longitude": "47.816700",
+        "phone": "+7 (927) 220-47-06",
+        "website": "https://www.kamrti.ru",
+        "tags": []
+    },
+    {
+        "id": 3,
+        "parts_available": [
+            {
+                "name": "Mersedes_457_LA"
+            }
+        ],
+        "name": "Автосодействие",
+        "image": null,
+        "address": "г. Курск, ул. Литовская д.12а",
+        "latitude": "51.704700",
+        "longitude": "36.171300",
+        "phone": "+7 (4712) 74‒06‒03",
+        "website": "https://автосодействие.рф",
+        "tags": []
+    },
+    {
+        "id": 4,
+        "parts_available": [
+            {
+                "name": "KAMAZ_740"
+            }
+        ],
+        "name": "Авто-альянс",
+        "image": null,
+        "address": "г. Люберцы, ул. Мира, д. 8-Б\"",
+        "latitude": "55.669300",
+        "longitude": "37.902600",
+        "phone": "7 (495) 109-14-79",
+        "website": "https://www.autoopt.ru/",
+        "tags": []
+    },
+    {
+        "id": 5,
+        "parts_available": [
+            {
+                "name": "YAMZ"
+            }
+        ],
+        "name": "Автоторгцентр",
+        "image": null,
+        "address": "пр. Машиностроителей, 1Ч, Чебоксары",
+        "latitude": "56.125100",
+        "longitude": "47.308000",
+        "phone": "7 (905) 029-74-44",
+        "website": "",
+        "tags": []
+    },
+    {
+        "id": 6,
+        "parts_available": [
+            {
+                "name": "KAMAZ_740"
+            },
+            {
+                "name": "YAMZ"
+            },
+            {
+                "name": "KAMAZ_R6"
+            },
+            {
+                "name": "Cummins_6.7"
+            }
+        ],
+        "name": "Авторемкомплект",
+        "image": null,
+        "address": "г. Ярославль, ул. проспект Октября 89, литер3, помещение 2.",
+        "latitude": "57.657800",
+        "longitude": "39.842900",
+        "phone": "+7 (920) 102-90-57\"",
+        "website": "https://tpkark.ru/",
+        "tags": []
+    }
+]
+
+export const citiesArray = ["Москва", "Санкт-Петербург", "Ярославль", "Чебоксары", "Люберцы", "Курск", "Балаково"]
+
+
+
+/*  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+ */
--- script.js
+++ script.js
@@ -1,120 +1,5 @@
-
-const storeList = [
-    {
-        "id": 1,
-        "parts_available": [
-            {
-                "name": "YAMZ"
-            },
-            {
-                "name": "KAMAZ_R6"
-            },
-            {
-                "name": "Cummins_6.7"
-            }
-        ],
-        "name": "РТИ",
-        "image": null,
-        "address": "Чебоксары, улица Цивильская, дом 13",
-        "latitude": "56.124145",
-        "longitude": "47.271371",
-        "phone": "4545454",
-        "website": "",
-        "tags": []
-    },
-    {
-        "id": 2,
-        "parts_available": [
-            {
-                "name": "Cummins_6.7"
-            }
-        ],
-        "name": "КАМРТИ",
-        "image": null,
-        "address": "Транспортная ул., 1, Балаково, Саратовская обл., 413859",
-        "latitude": "52.000700",
-        "longitude": "47.816700",
-        "phone": "+7 (927) 220-47-06",
-        "website": "https://www.kamrti.ru",
-        "tags": []
-    },
-    {
-        "id": 3,
-        "parts_available": [
-            {
-                "name": "Mersedes_457_LA"
-            }
-        ],
-        "name": "Автосодействие",
-        "image": null,
-        "address": "г. Курск, ул. Литовская д.12а",
-        "latitude": "51.704700",
-        "longitude": "36.171300",
-        "phone": "+7 (4712) 74‒06‒03",
-        "website": "https://автосодействие.рф",
-        "tags": []
-    },
-    {
-        "id": 4,
-        "parts_available": [
-            {
-                "name": "KAMAZ_740"
-            }
-        ],
-        "name": "Авто-альянс",
-        "image": null,
-        "address": "г. Люберцы, ул. Мира, д. 8-Б\"",
-        "latitude": "55.669300",
-        "longitude": "37.902600",
-        "phone": "7 (495) 109-14-79",
-        "website": "https://www.autoopt.ru/",
-        "tags": []
-    },
-    {
-        "id": 5,
-        "parts_available": [
-            {
-                "name": "YAMZ"
-            }
-        ],
-        "name": "Автоторгцентр",
-        "image": null,
-        "address": "пр. Машиностроителей, 1Ч, Чебоксары",
-        "latitude": "56.125100",
-        "longitude": "47.308000",
-        "phone": "7 (905) 029-74-44",
-        "website": "",
-        "tags": []
-    },
-    {
-        "id": 6,
-        "parts_available": [
-            {
-                "name": "KAMAZ_740"
-            },
-            {
-                "name": "YAMZ"
-            },
-            {
-                "name": "KAMAZ_R6"
-            },
-            {
-                "name": "Cummins_6.7"
-            }
-        ],
-        "name": "Авторемкомплект",
-        "image": null,
-        "address": "г. Ярославль, ул. проспект Октября 89, литер3, помещение 2.",
-        "latitude": "57.657800",
-        "longitude": "39.842900",
-        "phone": "+7 (920) 102-90-57\"",
-        "website": "https://tpkark.ru/",
-        "tags": []
-    }
-]
-
-const citiesArray = ["Москва", "Санкт-Петербург", "Ярославль", "Чебоксары", "Люберцы", "Курск", "Балаково"]
-
+import { citiesArray } from './constants.js'
+import { storeList } from './constants.js'
 
 
 let fetchedData
@@ -131,15 +16,40 @@ fetch('http://158.160.154.213/api/partners/')
 
 /* MAP */
 
+const searchInput = document.getElementById('city-input');
+
 function init() {
+    const partnersListButton = document.getElementById('partners-list-button')
+    const popupPartnersList = document.getElementById('popup-partners-list')
+    const closeButtonPartnersList = document.getElementsByClassName("popup-filter__close-button")[3];
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+
+        let filteredStoreList = [];
+
         const map = new ymaps.Map('map', {
             center: [position.coords.latitude, position.coords.longitude],
             zoom: 8,
             controls: ['smallMapDefaultSet', 'routeButtonControl']
         });
 
-        let filteredStoreList = [];
+        map.controls.remove('trafficControl')
+        map.controls.remove('typeSelector');
+        map.controls.remove("taxi")
+
+        const control = map.controls.get('routeButtonControl')
+        control.routePanel.state.set({
+            type: 'auto',
+            fromEnabled: true,
+            toEnabled: true,
+        })
+        control.routePanel.options.set({
+            types: {
+                auto: true,
+                pedestrian: true,
+            }
+        })
 
         function findCity(city) {
             const array = filteredStoreList.length == 0 ? storeList : filteredStoreList
@@ -158,18 +68,6 @@ function init() {
                     console.log('Ошибка геокодирования:', error);
                 });
         }
-
-        /* citiesArray.map(city => {
-            const cityName = document.createElement('li');
-            cityName.textContent = city;
-            citiesList.appendChild(cityName);
-            cityName.addEventListener('click', function () {
-                cityFilterPopup.style.display = "none";
-                getCenter(city)
-            })
-        }) */
-
-        const searchInput = document.getElementById('city-input');
 
         function createPartnersList(address) {
             const li = document.createElement('li');
@@ -210,26 +108,6 @@ function init() {
         filterCities()
         searchInput.addEventListener('input', filterCities);
 
-        map.controls.remove('trafficControl')
-        map.controls.remove('typeSelector');
-        map.controls.remove("taxi")
-
-        const control = map.controls.get('routeButtonControl')
-        control.routePanel.state.set({
-            type: 'auto',
-            fromEnabled: true,
-            toEnabled: true,
-        })
-
-        control.routePanel.options.set({
-            types: {
-                auto: true,
-                pedestrian: true,
-            }
-        })
-
-
-
         function addPlacemark(storeList) {
             storeList.forEach(store => {
                 const placemark = new ymaps.Placemark([store.latitude, store.longitude], {
@@ -261,14 +139,7 @@ function init() {
                     });
                 });
                 map.geoObjects.add(placemark);
-
                 createPartnersList(store.address)
-
-
-                const partnersListButton = document.getElementById('partners-list-button')
-                const popupPartnersList = document.getElementById('popup-partners-list')
-                const closeButtonPartnersList = document.getElementsByClassName("popup-filter__close-button")[3];
-
 
                 partnersListButton.addEventListener("click", function () {
                     popupPartnersList.style.display = "block";
@@ -282,11 +153,8 @@ function init() {
 
         addPlacemark(storeList)
 
-
-        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-
         function filterStoreList() {
-            const selectedParts = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(checkbox => checkbox.value);
+            const selectedParts = Array.from(document.querySelectorAll('.popup-filter__engine-checkbox:checked')).map(checkbox => checkbox.value);
             filteredStoreList = storeList.filter(store => {
                 return selectedParts.some(selectedPart => {
                     return store.parts_available.some(part => part.name === selectedPart);
@@ -309,19 +177,18 @@ function init() {
         }
     })
 }
-
 ymaps.ready(init);
 
 /* Бургер Меню */
 
 const popupMenu = document.querySelector(".popup-menu")
-
 const burgerButton = document.getElementById('header-nav-burger');
+const popupCloseButton = document.getElementById('popup-menu-close');
+
 burgerButton.addEventListener('click', () => {
     popupMenu.style.display = "block";
 });
 
-const popupCloseButton = document.getElementById('popup-menu-close');
 popupCloseButton.addEventListener('click', () => {
     popupMenu.style.display = "none";
 });
@@ -329,24 +196,12 @@ popupCloseButton.addEventListener('click', () => {
 /* Фильтр по типу двигателя */
 
 const filterPopup = document.getElementById("popup-engine-filter");
-const closeButtonEngine = document.getElementsByClassName("popup-filter__close-button")[0];
 const engineFilterButton = document.getElementById("engine-filter");
 const submitEngineFilterButton = document.getElementById("engine-filter-submit-button");
 
 engineFilterButton.addEventListener("click", function () {
     filterPopup.style.display = "block";
 });
-
-closeButtonEngine.addEventListener("click", function () {
-    filterPopup.style.display = "none";
-});
-
-/* window.addEventListener("touchstart", function (event) {
-    if (event.touches.clientY < 50) {
-        event.preventDefault()
-        filterPopup.style.display = "none";
-    }
-}); */
 
 submitEngineFilterButton.addEventListener("click", function (event) {
     event.preventDefault()
@@ -355,19 +210,28 @@ submitEngineFilterButton.addEventListener("click", function (event) {
 
 /* Фильтр по партнерам */
 
-
 const partnersFilterPopup = document.getElementById("popup-partners-filter");
 const partnersFilterButton = document.getElementById("partner-filter");
-const closeButtonPartners = document.getElementsByClassName("popup-filter__close-button")[1];
-
+const toggleButton = document.getElementById('toggleButton')
+const partnersSubmitButton = document.getElementById('partners-filter-submit-button')
 
 partnersFilterButton.addEventListener("click", function () {
     partnersFilterPopup.style.display = "block";
 });
 
-closeButtonPartners.addEventListener("click", function () {
-    partnersFilterPopup.style.display = "none";
+toggleButton.addEventListener('change', function () {
+    if (this.checked) {
+        console.log('Бегунок активен');
+    } else {
+        console.log('Бегунок не активен');
+    }
 });
+
+partnersSubmitButton.addEventListener('click', function (e) {
+    e.preventDefault()
+    partnersFilterPopup.style.display = "none";
+    console.log('Здесь еще что-то будет')
+})
 
 /* Фильтр по городам */
 
@@ -375,9 +239,6 @@ const cityFilterPopup = document.getElementById("popup-city-filter");
 const cityFilterButton = document.getElementById("city-filter");
 const closeButtonCity = document.getElementsByClassName("popup-filter__close-button")[2];
 const citiesList = document.getElementById('cities-list')
-
-
-const searchInput = document.getElementById('city-input');
 
 cityFilterButton.addEventListener("click", function () {
     cityFilterPopup.style.display = "block";
@@ -392,25 +253,25 @@ closeButtonCity.addEventListener("click", function () {
 
 const partnersListButton = document.getElementById('partners-list-button')
 const popupPartnersList = document.getElementById('popup-partners-list')
-const closeButtonPartnersList = document.getElementsByClassName("popup-filter__close-button")[3];
 const partnersListContainer = document.getElementById('partners-list')
-
-
 
 partnersListButton.addEventListener("click", function () {
     popupPartnersList.style.display = "block";
 });
 
-closeButtonPartnersList.addEventListener("click", function () {
-    popupPartnersList.style.display = "none";
-});
 
-
-/* Закрытия попапов по клику на overlay */
+/* Закрытие попапов */
 
 const popup = document.querySelectorAll('.popup-filter__content');
 const overlay = document.querySelectorAll('.popup-filter');
 const closeButtons = document.querySelectorAll('.popup-filter__close-button');
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const overlay = button.closest('.popup-filter');
+        overlay.style.display = "none";;
+    });
+});
 
 overlay.forEach(item => {
     item.addEventListener('click', function () {
@@ -427,70 +288,14 @@ popup.forEach(item => {
 
 
 
+/* citiesArray.map(city => {
+    const cityName = document.createElement('li');
+    cityName.textContent = city;
+    citiesList.appendChild(cityName);
+    cityName.addEventListener('click', function () {
+        cityFilterPopup.style.display = "none";
+        getCenter(city)
+    })
+}) */
 
-/*  map.controls.remove('geolocationControl'); // удаляем геолокацию
-  map.controls.remove('searchControl'); // удаляем поиск
-  map.controls.remove('trafficControl'); // удаляем контроль трафика
-  map.controls.remove('typeSelector'); // удаляем тип
-  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-  map.controls.remove('rulerControl'); // удаляем контрол правил
-  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
- */
 
-// эксперименты со смахиванием (не удачные):
-
-/*let startY = 0;
-const touchThreshold = 100; // Порог для определения жеста смахивания
-
-document.addEventListener('touchstart', (event) => {
-  startY = event.touches[0].clientY;
-});
-
-document.addEventListener('touchmove', (event) => {
-  const currentY = event.touches[0].clientY;
-  const deltaY = currentY - startY;
-
-  // Проверяем, превышает ли смещение по вертикали пороговое значение
-  if (Math.abs(deltaY) > touchThreshold) {
-    // Предотвращаем стандартное поведение браузера
-    event.preventDefault();
-  }
-});
-
-----------------
-
-  let isPopupOpen = false;
-    let startY = 0;
-    const popupElement = document.querySelector('.popup-filter__content');
-    const overlayElement = document.querySelector('.popup-filter');
-
-    function openPopup() {
-      popupElement.style.display = 'block';
-      overlayElement.style.display = 'block';
-      isPopupOpen = true;
-    }
-
-    function closePopup() {
-      popupElement.style.display = 'none';
-      overlayElement.style.display = 'none';
-      isPopupOpen = false;
-    }
-
-    popupElement.addEventListener('touchstart', (event) => {
-      if (isPopupOpen) {
-        startY = event.touches[0].clientY;
-      }
-    });
-
-    popupElement.addEventListener('touchmove', (event) => {
-      if (isPopupOpen) {
-        const currentY = event.touches[0].clientY;
-        const deltaY = currentY - startY;
-
-        // Проверяем, был ли выполнен жест смахивания вниз
-        if (deltaY > 100) {
-          closePopup();
-        }
-      }
-    }); */
